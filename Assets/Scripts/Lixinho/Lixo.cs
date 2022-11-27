8fd6cc2dc50af8ad815d51d96c495d5d22aa4351
Laryssa Maciel <maciel.f.laryssa@gmail.com>
2022-11-27 16:42:33 -0300

    ajuste selecao da lixeira
    lixeria só é selecionada ao estar segurando um lixo, minigame do lixo

--- Assets/Scripts/Lixinho/Lixo.cs
+++ Assets/Scripts/Lixinho/Lixo.cs
@@ -12,81 +12,42 @@ public class Lixo : MonoBehaviour
     private Draggable dragcs;
     public bool x = true, canDo = false;
 
-    // public Color objectColor;
-    // public Color fadeColor = Color.red;
-    // private Color targetColor = Color.white;
-    // public float fadeTime = 1f;
-    // public float fadeStart = 0, timePassed;
-
     void Awake() => dragcs = GetComponent<Draggable>();
     
     void Update() 
     {
-        // if (y == 1) 
-        // {
-        //     if (timePassed > 0) // fade white
-        //     {
-        //         timePassed -= Time.deltaTime;
-        //         colisao.gameObject.transform.Find("Lixeira").gameObject.GetComponent<MeshRenderer>().material.color 
-        //             = Color.Lerp(objectColor, objectColor, timePassed / fadeTime);
-        //     }
-        //     else
-        //     {
-        //         y = 2; // null
-        //     }
-        // }
-
-        // if (y == 0)
-        // {
-        //     if (timePassed < fadeTime) // fade red
-        //     {
-        //         timePassed += Time.deltaTime;
-        //         colisao.gameObject.transform.Find("Lixeira").gameObject.GetComponent<MeshRenderer>().material.color 
-        //             = Color.Lerp(objectColor, fadeColor, timePassed / fadeTime);
-        //     }
-        //     else if (timePassed >= fadeTime)
-        //     {
-        //         y = 1; // fade white
-        //     }
-        // }
-
-        if (ponto[0] != null)
+        if (ponto[0] != null) // se não saiu do ponto de spawn / não lançou lixo
         {
-            if (this.passou == 0 && this.tipo == this.colisao.name && dragcs.m == 1) // vai lansar o lixo
-            { this.canDo = true; } 
+            if (this.passou == 0 && this.tipo == this.colisao.name && dragcs.m == 1) // vai lançar o lixo
+            { this.canDo = true; } // pode enviar lixo
             else if (this.passou == 0 && this.tipo != this.colisao.name)
-            { this.canDo = false; }
-
-            // if (this.passou == 0 && tipo != colisao.name && y == -1)
-            // {
-            //     y = 0; // fade red
-            // }
+            { this.canDo = false; } // não pode enviar lixo
 
             if (this.canDo == true)
-            {
+            {   // move para cima da lixeira
                 this.transform.position = Vector3.MoveTowards(transform.position, ponto[0].position, 3 * Time.deltaTime); // fica no ponto inicial
-                if (this.x == true)
-                {
+                if (this.x == true) // acertou
+                {   // efeito de partícula
                     this.colisao.gameObject.transform.Find("ringParticle").GetComponent<ParticleSystem>().Play();
-                    this.x = false;
+                    this.x = false; 
                 }
             }
-
-            if (this.gameObject.transform.position == ponto[0].position) { this.passou = 1; } // vai pro proximo ponto
+            // vai pro proximo ponto 
+            if (this.gameObject.transform.position == ponto[0].position) { this.passou = 1; } 
             
             if (this.passou == 1)
-            {
+            {   // vai pra dentro da lixeira
                 //this.gameObject.GetComponent<BoxCollider>().enabled = false;
                 this.transform.position = Vector3.MoveTowards(transform.position, ponto[1].position, 10 * Time.deltaTime); // vai pro ponto 2
             }
             
-            if (colisao_.name.Contains("destroi")) 
+            if (colisao_.name.Contains("destroi")) // destroi o lixo
             { 
                 this.colisao.gameObject.transform.Find("Lixeira").gameObject.GetComponent<Animator>().SetInteger("s", 0);
                 Destroy(this.gameObject); // destroi desintegra oblitera ele :)
             }
 
-            if (this.transform.position.y >= 2)
+            if (this.transform.position.y >= 2) // bug fix lixo voando para cima
             {
                 this.gameObject.GetComponent<Rigidbody>().useGravity = true;
             }
@@ -108,7 +69,6 @@ public class Lixo : MonoBehaviour
         }
         mouseStatus = 2; 
     }
-    //
 
     void OnTriggerEnter(Collider col)
     {
@@ -116,10 +76,8 @@ public class Lixo : MonoBehaviour
         if (col.name.Contains("Lixeira"))
         {
             colisao = col;
-            // y = -1;
             this.gameObject.GetComponent<Rigidbody>().useGravity = true;
-            col.gameObject.transform.Find("Lixeira").gameObject.GetComponent<Animator>().SetInteger("s", 1);; // chama a animacao
-            // objectColor = col.gameObject.transform.Find("Lixeira").gameObject.GetComponent<MeshRenderer>().material.color;
+            
         }
     }
 
@@ -128,6 +86,11 @@ public class Lixo : MonoBehaviour
         if (col.name.Contains("Lixeira"))
         {
             colisao = col;
+
+            if (dragcs.beingDrag == true)
+            {
+                col.gameObject.transform.Find("Lixeira").gameObject.GetComponent<Animator>().SetInteger("s", 1);; // chama a animacao
+            }
             
             if (mouseStatus == 2) // se soltou o mouse
             {
@@ -143,8 +106,6 @@ public class Lixo : MonoBehaviour
         if (col.name.Contains("Lixeira"))
         {
             col.gameObject.transform.Find("Lixeira").gameObject.GetComponent<Animator>().SetInteger("s", 0);
-            
-            // if (y == 2) { y = 0; }
         }
     }
 }
