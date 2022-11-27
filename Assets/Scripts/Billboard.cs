a5374140ea85bcae53d4a7abcbade153cac40d43
Laryssa Maciel <maciel.f.laryssa@gmail.com>
2022-11-27 19:07:24 -0300

    ajuste nos icones da cidade

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
 public class Billboard : MonoBehaviour
 {
     public GameObject player, t1;
     public Fade fadecs;
-    private bool colideLixo = false;
-    public float distancia = 9f;
+    public float distancia = 5f;
 
     private Controles controles;
 
@@ -22,18 +22,18 @@ public class Billboard : MonoBehaviour
 
     void Update() 
     {
+        transform.LookAt(Camera.main.transform.position, Vector3.up);
+
         if (Vector3.Distance(this.transform.position, this.player.transform.position) < distancia)
         {
-            transform.LookAt(Camera.main.transform.position, Vector3.up);
-            this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            this.gameObject.transform.GetChild(0).GetComponent<Image>().color = Color.green;
             Interagiu();
         }
-        else { this.gameObject.transform.GetChild(0).gameObject.SetActive(false); }
     }
 
     void Interagiu()
     {
-        if (colideLixo == false && controles.ActionMap.Interagir.ReadValue<float>() > 0)
+        if (controles.ActionMap.Interagir.ReadValue<float>() > 0)
         {
             switch (this.gameObject.transform.parent.tag) 
             {
@@ -60,11 +60,5 @@ public class Billboard : MonoBehaviour
                 break;
             }
         }
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag != "lixo") colideLixo = false; 
-        else colideLixo = true; 
     }
 }
