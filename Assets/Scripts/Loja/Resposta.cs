70fa50a3853bd606c93048a93cd0cb9c2b7d7056
Laryssa Maciel <maciel.f.laryssa@gmail.com>
2022-12-03 16:12:19 -0300

    feature feedback positivo minigame loja

--- Assets/Scripts/Loja/Resposta.cs
+++ Assets/Scripts/Loja/Resposta.cs
@@ -7,6 +7,7 @@ public class Resposta : MonoBehaviour
 {
     public int num_;
     private Caixa box;
+    public GameObject[] opcs;
     
     void Awake() => box = GameObject.FindGameObjectWithTag("caixa").GetComponent<Caixa>();
 
@@ -16,17 +17,33 @@ public class Resposta : MonoBehaviour
         {
             print("certo");
 
+            this.gameObject.transform.GetChild(2).GetComponent<ParticleSystem>().Play();
+
             GameObject[] lanches = GameObject.FindGameObjectsWithTag("lanche");
             foreach (GameObject item in lanches) { Destroy(item); }
 
             box.total = 0;
             box.next = 0;
             box.repeticao++;
-            box.opc.SetActive(false);
+            StartCoroutine("Wait", .4f);
         }
         else
         {
             print("errado");
         }
+    }
+
+    IEnumerator Wait()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            opcs[i].gameObject.GetComponent<Button>().interactable = false;
+        }
+        yield return new WaitForSeconds(.4f);
+        for (int i = 0; i < 3; i++)
+        {
+            opcs[i].gameObject.GetComponent<Button>().interactable = true;
+        }
+        box.opc.SetActive(false);
     }
 }
